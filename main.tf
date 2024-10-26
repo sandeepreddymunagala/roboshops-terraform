69ab3b4fe1af239ff30e62eacd411b3a7b1d5831
sandeep <munagalasandeepreddy@gamail.com>
2024-10-26 18:05:46 +0200

    learn terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
-module "instance"{
-  source = "git::https://github.com/sandeepreddymunagala/tf-modules-app.git"
-  for_each = var.components
-  component = each.key
-  env = var.env
+#module "instance"{
+# source = "git::https://github.com/sandeepreddymunagala/tf-modules-app.git"
+#  for_each = var.components
+#  component = each.key
+#  env = var.env
+#}
+
+variable "sample" {
+  default = 100
+}
+variable "sample1"{
+  default = "Hello World"
+}
+output "sample" {
+  value = var.sample
+}
+output "sample1" {
+  value = var.sample1
 }
